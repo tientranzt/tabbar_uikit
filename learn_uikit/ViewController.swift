1da4b7f3bd54de3970551d541beeaf9ac293a2f4
tientran <tientran.home@gmail.com>
2020-12-26 19:22:01 +0700

    Initial Commit

---
+++ learn_uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  learn_uikit
+//
+//  Created by tientran on 26/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
